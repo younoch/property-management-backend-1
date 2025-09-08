aca8d78c909f2202843596c57422debc0c97162b
younoch <younoch295@outlook.com>
2025-09-08 22:21:08 +0600

    fixt s df asdf

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,5 +1,6 @@
 import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersManagementController } from './users.management.controller';
@@ -10,9 +11,21 @@ import { CurrentUserMiddleware } from './middlewares/current-user.middleware';
 import { TokenRefreshInterceptor } from './interceptors/token-refresh.interceptor';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), ConfigModule],
+  imports: [
+    TypeOrmModule.forFeature([User]), 
+    ConfigModule,
+    JwtModule.register({
+      secret: process.env.JWT_ACCESS_SECRET,
+      signOptions: { expiresIn: process.env.JWT_ACCESS_EXPIRES_IN || '15m' },
+    }),
+  ],
   controllers: [UsersController, UsersManagementController],
-  providers: [UsersService, AuthService, TokenRefreshInterceptor],
+  providers: [
+    UsersService, 
+    AuthService, 
+    TokenRefreshInterceptor,
+    JwtService,
+  ],
 })
 export class UsersModule {
   configure(consumer: MiddlewareConsumer) {
